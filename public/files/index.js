ecc13014511ad35204d5c800a47ff69ce847bf5a
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-06-22 00:21:56 +0530

    first commit

---
+++ public/files/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
